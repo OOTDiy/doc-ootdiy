3cbf7f7048c214f8681a6f42492a2f89cbea87dc
lusis <lusisgrt582@gmail.com>
2025-03-21 06:46:46 +0700

    Menghapus SEAQIS dan phet-main dari tracking Git

--- seaqisPhet-main/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import HomeView from '@/views/HomeView.vue'
-import HomeHeader from '@/components/layout/HeaderHome.vue'
-</script>
-
-<template>
-  <HomeHeader/>
-  <HomeView />
-</template>
